55f9c2223f5102b0ef112a3947adbcd0a1aa3c2e
selirah <selirah@pces.mk>
2022-11-27 21:38:57 +0000

    nav menu icons only

--- components/nav-menu/index.tsx
+++ components/nav-menu/index.tsx
@@ -14,7 +14,8 @@ export type SubLinkObj = {
 interface NavMenuProps {
   menuTitle: string
   active?: boolean
-  IconSVG?: React.FC<React.SVGProps<SVGSVGElement>>
+  IconSVG: React.FC<React.SVGProps<SVGSVGElement>>
+  iconOnly?: boolean
 }
 
 interface BasicProps extends NavMenuProps {
@@ -26,8 +27,8 @@ interface SubLinksProps extends NavMenuProps {
 }
 
 export const Basic: React.FC<BasicProps> = (props) => {
-  const { menuTitle, IconSVG, active, link } = props
-  return (
+  const { menuTitle, IconSVG, active, link, iconOnly } = props
+  return !iconOnly ? (
     <div
       className={classnames(
         'p-[12px] rounded font-montserrat cursor-pointer hover:bg-primary hover:rounded font-regular hover:text-light-btnText common-transition',
@@ -38,16 +39,30 @@ export const Basic: React.FC<BasicProps> = (props) => {
       )}
     >
       <Link href={link} className="flex items-center">
-        {IconSVG ? <IconSVG className="w-[18px] h-[18px] mr-2" /> : null}
+        <IconSVG className="w-[18px] h-[18px] mr-2" />
         <span className="text-pNormal">{menuTitle}</span>
+      </Link>
+    </div>
+  ) : (
+    <div
+      className={classnames(
+        'flex justify-center items-center p-[12px] w-[70px] h-[56px] cursor-pointer hover:bg-primary hover:rounded  hover:text-light-btnText common-transition',
+        {
+          'text-light-text dark:text-dark-text ': !active,
+          'bg-primary text-light-btnText': active
+        }
+      )}
+    >
+      <Link href={link} className="flex items-center">
+        <IconSVG className="w-[24px] h-[24px]" />
       </Link>
     </div>
   )
 }
 
 export const SubLink: React.FC<SubLinksProps> = (props) => {
-  const { menuTitle, subLinks, IconSVG, active } = props
-  return (
+  const { menuTitle, subLinks, IconSVG, active, iconOnly } = props
+  return !iconOnly ? (
     <div className="group inline-block relative w-full">
       <div
         className={classnames(
@@ -59,12 +74,45 @@ export const SubLink: React.FC<SubLinksProps> = (props) => {
         )}
       >
         <div className="flex items-center">
-          {IconSVG ? <IconSVG className="w-[18px] h-[18px] mr-2" /> : null}
+          <IconSVG className="w-[18px] h-[18px] mr-2" />
           <span className="text-pNormal">{menuTitle}</span>
         </div>
         <ChevronRightIcon className="w-4 h-4 invisible group-hover:visible" />
       </div>
       <ul className="w-64 absolute origin-top-right -right-[260px] top-0 hidden group-hover:block rounded border border-light-border dark:border-dark-border shadow-penumbra bg-light-container dark:bg-dark-container font-montserrat transition ease-out duration-100 text-pSmall">
+        {subLinks.map((link) => (
+          <li key={link.link} className="">
+            <Link className="" href={link.link}>
+              <button
+                className="flex w-full items-center disabled:cursor-not-allowed text-light-text dark:text-dark-text hover:text-light-btnText hover:bg-primary rounded p-[12px] common-transition disabled:hover:bg-primaryLight"
+                disabled={link.inaccessible}
+              >
+                {link.IconSVG ? (
+                  <link.IconSVG className="mr-2 w-[16px] h-[16px] " aria-hidden="true" />
+                ) : null}
+                {link.title}
+              </button>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  ) : (
+    <div className="group inline-block relative w-full">
+      <div
+        className={classnames(
+          'flex justify-center items-center p-[12px] w-[70px] h-[56px] rounded cursor-pointer group-hover:bg-primary group-hover:text-light-btnText hover:bg-primary hover:rounded hover:text-light-btnText common-transition',
+          {
+            'text-light-text dark:text-dark-text ': !active,
+            'bg-primary text-light-btnText': active
+          }
+        )}
+      >
+        <div className="flex items-center">
+          <IconSVG className="w-[24px] h-[24px]" />
+        </div>
+      </div>
+      <ul className="w-64 absolute origin-top-right -right-[110px] top-0 hidden group-hover:block rounded border border-light-border dark:border-dark-border shadow-penumbra bg-light-container dark:bg-dark-container font-montserrat transition ease-out duration-100 text-pSmall">
         {subLinks.map((link) => (
           <li key={link.link} className="">
             <Link className="" href={link.link}>
